3c620aece2707d9f7b65b3a60ca7497f49f7dc10
Sandhya Rao <smudra@yahoo.com>
2018-02-22 16:49:56 -0600

    adding index area and themepark files

--- js/main.js
+++ js/main.js
@@ -2,4 +2,14 @@
 
 let themeParkArea = require("./area.js");
 
+themePark.loadThemeParkAreas()
+.then(
+	(themeAreaLoadResolve) => {
+		console.log("Cake Promise", themeAreaLoadResolve);
+		populatePage(themeAreaLoadResolve);
+	},
+	(reject) => {
+		console.log("Areas are not loading");
+	});
+
 // themeParkArea.loadThemeParkAreas();
